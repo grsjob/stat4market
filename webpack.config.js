734b9214669cf17310dc063aa91a87006bcfb47d
grsjob <grsgithub@gmail.com>
2022-05-05 01:21:01 +0300

    add styles course and experience
    descktop xl styles

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+let mode = 'development'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production'
+}
+console.log(mode + '  mode')
+module.exports = {
+    entry: '/src/index.js',
+    mode: mode,
+    output: {
+        assetModuleFilename: "assets/[hash][ext][query]",
+        clean: true,
+    },
+    devtool: 'source-map',
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: "education.html"
+        })],
+
+    module: {
+        rules: [
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    (mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
